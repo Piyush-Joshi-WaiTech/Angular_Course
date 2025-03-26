b85802e2e7b139d5cecaa4574802afe06d95b2eb
Piyush-Joshi-WaiTech <piyush.joshi@waiin.com>
2025-03-26 15:19:00 +0530

    Add signup and profile components with styles and tests; update app component and index.html for Bootstrap integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-tut';
+  count = 0;
+
+  handleCounter(val: string) {
+    if (val === 'minus') {
+      this.count--;
+    } else if (val === 'plus') {
+      this.count++;
+    } else {
+      this.count = 0;
+    }
+  }
 }
