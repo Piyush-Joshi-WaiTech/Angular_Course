73561a5f4dd4c7ca6b43e39ed6a253b709c82c87
Piyush-Joshi-WaiTech <piyush.joshi@waiin.com>
2025-03-27 11:04:43 +0530

    Implement signal-based state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -16,5 +17,31 @@ export class AppComponent {
     } else {
       this.count = 0;
     }
+  }
+
+  count1 = signal(10);
+  x = 20;
+  constructor() {
+    effect(() => {
+      console.log('count1:', this.count1());
+    });
+  }
+
+  updateValues() {
+    this.count1.set(this.count1() + 10);
+    this.x += 1;
+  }
+
+  data: WritableSignal<number | string> = signal(10);
+  updateSignal() {
+    this.data.set('Hello');
+  }
+
+  a = 10;
+  b = 20;
+  z = this.a + this.b;
+
+  showValue() {
+    console.log(this.z);
   }
 }
