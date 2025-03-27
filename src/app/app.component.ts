814a1385f9c1e1d883b945d426d36c906b58a371
Piyush-Joshi-WaiTech <piyush.joshi@waiin.com>
2025-03-27 11:41:19 +0530

    Add user name management and display logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,39 @@ import { Component, effect, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   count = 0;
+  count1 = signal(10);
+  x = 20;
+  data: WritableSignal<number | string> = signal(10);
+
+  a = 10;
+  b = 20;
+  z = this.a + this.b;
+
+  userName = signal('Piyush');
+  count2 = signal(0);
+  displayHeading = signal(false);
+
+  constructor() {
+    effect(() => {
+      console.log('count1:', this.count1());
+    });
+
+    effect(() => {
+      if (this.count2() >= 2) {
+        this.displayHeading.set(true);
+        setTimeout(() => {
+          this.displayHeading.set(false);
+        }, 2000);
+      } else {
+        this.displayHeading.set(false);
+      }
+    });
+  }
 
   handleCounter(val: string) {
     if (val === 'minus') {
@@ -19,29 +46,24 @@ export class AppComponent {
     }
   }
 
-  count1 = signal(10);
-  x = 20;
-  constructor() {
-    effect(() => {
-      console.log('count1:', this.count1());
-    });
-  }
-
   updateValues() {
     this.count1.set(this.count1() + 10);
     this.x += 1;
   }
 
-  data: WritableSignal<number | string> = signal(10);
   updateSignal() {
     this.data.set('Hello');
   }
 
-  a = 10;
-  b = 20;
-  z = this.a + this.b;
-
   showValue() {
     console.log(this.z);
+  }
+
+  updateUserName() {
+    this.userName.set('Updated Piyush');
+    this.count2.set(this.count2() + 1);
+  }
+  incrementCount() {
+    this.count2.set(this.count2() + 1);
   }
 }
